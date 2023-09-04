0c7c733cfe2442b538e0a2f341feccd2d5b36b08
Juan Ignacio Grodz <jigrodz@gmail.com>
2023-09-04 16:26:47 -0300

    se termino estilos navbar

--- src/Components/pageComponents/Nav.jsx
+++ src/Components/pageComponents/Nav.jsx
@@ -4,12 +4,13 @@ export default function Nav() {
     return (
         <nav className="navbar">
         <div className="navbar-left">
-          <a href="#" className="logo">Logo</a>
-        </div>
+        <img src='/logo.png' alt="logo header"/>
+          </div>
         <div className="navbar-right">
-          <a href="#" className="nav-item">Item 1</a>
-          <a href="#" className="nav-item">Item 2</a>
-          <a href="#" className="nav-item">Item 3</a>
+          <a href="#" className="nav-item">Sobre nosotros</a>
+          <a href="#" className="nav-item">Carga CV</a>
+          <a href="#" className="nav-item">Buscar Empleo</a>
+          <a href="#" className="nav-item">Ingresar</a>
           <div className="mobile-menu">
             <div className="mobile-icon"></div>
           </div>
