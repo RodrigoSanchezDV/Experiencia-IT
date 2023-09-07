d05a4a7a28373fb278a986c4f0394c613a2f1df0
EzeTorcetta <ezequiel.torcetta.97@gmail.com>
2023-09-07 03:13:10 -0300

    Home(falta iconos de las cards, conectar los links con las url y algunos detalles de estilo)

--- src/Components/pageComponents/Footer.jsx
+++ src/Components/pageComponents/Footer.jsx
@@ -4,14 +4,27 @@ import "../styles/Footer.css";
 
 export default function Footer() {
     return (
-        <footer className="footer">
-            <div className="footer-icons">
-            <i class="fa-brands fa-facebook"></i>
-            <i class="fa-brands fa-instagram"></i>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-brands fa-twitter"></i>
+        <footer className="footer-container">
+            <div className="top-footer">
+                <div className="links-cont">
+                    <a href="#" >Sobre Nosotros</a>
+                    <a href="#" >Buscar Empleo</a>
+                    <a href="#" >Ingresar</a>
+                </div>
+
+                <div className="footer-icons">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                </div>
+
+                <img src="../../public/logo-footer.png" alt="logo-footer" />
             </div>
-            <p>2023 - Todos los derechos reservados</p>
+            <div className="bottom-footer">
+                <p>© Copyright 2023 | Diseño y Desarrollo</p>
+            </div>
+
         </footer>
     );
 }
--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -12,12 +12,12 @@ export default function Home() {
             <div className="img-container">
                 <img src={imgPrincipal} alt="imgPrincipal" />
                 <div className="text-overlay">
-                    <h1>¡Te damos la biencenida a Smart Resources!</h1>
+                    <h1>¡Te damos la bienvenida a Smart Resources!</h1>
                     <p>Tu viaje comienza aquí</p>
                 </div>
             </div>
             <div>
-                <p>Estamos encantados de que estés considerando unirte a nuestro equipo en Smart Resources! Nuestro proceso de incorporación está diseñado para brindarte una experiencia sin problemas mientras te preparas para un emocionante viaje en tu nueva carrera con nosotros.</p>
+                <p className="about-text">Estamos encantados de que estés considerando unirte a nuestro equipo en Smart Resources! Nuestro proceso de incorporación está diseñado para brindarte una experiencia sin problemas mientras te preparas para un emocionante viaje en tu nueva carrera con nosotros.</p>
                 <h3>Beneficios de trabajar con nosotros</h3>
                 <div className="cards-container">
                     <div className="card">
@@ -26,9 +26,9 @@ export default function Home() {
                             <i className="fa fa-icono"></i>
                         </div>
 
-                        <h2>Card 1</h2>
+                        <h2>Crecimiento Profesional</h2>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>En Smart Resources, te brindamos oportunidades para desarrollar tus habilidades y avanzar en tu carrera.</p>
                     </div>
 
                     <div className="card">
@@ -37,9 +37,9 @@ export default function Home() {
                             <i className="fa fa-icono"></i>
                         </div>
 
-                        <h2>Card 2</h2>
+                        <h2>Innovación</h2>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>Contribuirás a proyectos emocionantes y desafiantes que fomentan la innovación y la creatividad.</p>
                     </div>
 
                     <div className="card">
@@ -48,9 +48,9 @@ export default function Home() {
                             <i className="fa fa-icono"></i>
                         </div>
 
-                        <h2>Card 3</h2>
+                        <h2>Beneficios Completos</h2>
 
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>Ofrecemos una amplia gama de beneficios, desde seguro médico hasta programas de desarrollo personal.</p>
                     </div>
                 </div>
             </div>
@@ -59,74 +59,81 @@ export default function Home() {
             <div className="container-azul">
                 <div className="content-azul">
                     <div className="left-side-azul">
-                        <h2 className="h2-azul">Título</h2>
-                        <p className="p-azul">Texto descriptivo sobre el contenido de la tarjeta.</p>
-                        <button>Botón</button>
+                        <h2 className="h2-azul">¿Listo para comenzar tu viaje con nosotros?</h2>
+                        <p className="p-azul">Regístrate ahora y comienza tu proceso de incorporación. Esperamos darte la bienvenida Smart Resources y ser parte de tu crecimiento y éxito.</p>
+                        <button className="button-azul">Comenzar el Proceso de Registro</button>
                     </div>
                     <div className="right-side-azul">
-                        <img className="img-azul" src="ruta-de-tu-imagen.jpg" alt="Imagen" />
+                        <img className="img-azul" src="../../../public/img-azul.png" alt="Imagen" />
                     </div>
                 </div>
             </div>
 
             <div className="container-manos">
-                <div className="content-manos">
-                    <div className="left-side-azul">
-                        <h2 className="h2-manos">Título</h2>
-                        <img className="img-azul" src="ruta-de-tu-imagen.jpg" alt="Imagen" />
-                    </div>
+
+
+                <img className="img-manos" src="../../../public/img-manos.png" alt="Imagen" />
+                <div className="text-manos">
+                    <h2 className="h2-manos">Sobre Nosotros</h2>
+                    <p className="p-manos">En Smart Resources, creemos en el poder de la excelencia en la gestión de talento. Somos una plataforma de onboarding dedicada a conectar a empresas con profesionales talentosos y brindar un enfoque completo y efectivo para la incorporación de nuevos miembros a su equipo.</p>
                 </div>
+
+
             </div>
 
 
             <div className="cards-container">
-                    <div className="card">
+                <div className="card">
 
-                        <div className="card-icon">
-                            <i className="fa fa-icono"></i>
-                        </div>
-
-                        <h2>Card 1</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <div className="card-icon">
+                        <i className="fa fa-icono"></i>
                     </div>
 
-                    <div className="card">
+                    <h2>Nuestro Equipo</h2>
 
-                        <div className="card-icon">
-                            <i className="fa fa-icono"></i>
-                        </div>
-
-                        <h2>Card 2</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-
-                    <div className="card">
-
-                        <div className="card-icon">
-                            <i className="fa fa-icono"></i>
-                        </div>
-
-                        <h2>Card 3</h2>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
+                    <p>Detrás de Smart Resources, hay un equipo apasionado de profesionales de recursos humanos, desarrolladores de software y expertos en diseño que comparten una visión común: revolucionar la forma en que las empresas abordan la incorporación de nuevos empleados. Nos enorgullece trabajar en estrecha colaboración con nuestros clientes para brindar soluciones que marquen la diferencia.</p>
                 </div>
 
-                <div className="background-image">
+                <div className="card">
+
+                    <div className="card-icon">
+                        <i className="fa fa-icono"></i>
+                    </div>
+
+                    <h2>Nuestro Compromiso con la Excelencia</h2>
+
+                    <p>En Smart Resources, la excelencia es nuestro estándar. Nos esforzamos por brindar soluciones de onboarding de clase mundial que transformen la forma en que las empresas gestionan su talento. Nuestra plataforma está diseñada para ser intuitiva, eficiente y altamente personalizable para satisfacer sus necesidades únicas.</p>
+                </div>
+
+                <div className="card">
+
+                    <div className="card-icon">
+                        <i className="fa fa-icono"></i>
+                    </div>
+
+                    <h2>Nuestra Misión</h2>
+
+                    <p>Nuestra misión es facilitar la transición de candidatos a colaboradores valiosos y comprometidos. Creemos que cada nuevo empleado es una oportunidad para el crecimiento y el éxito de su empresa. Trabajamos incansablemente para simplificar y optimizar el proceso de incorporación, permitiéndole enfocarse en lo que hace mejor: hacer crecer su negocio.</p>
+                </div>
+            </div>
+
+            <div className="unase-container">
                 {/* Imagen a la izquierda */}
                 {/* <img src={imgIzquierda} alt="Imagen Izquierda" className="left-image" /> */}
                 {/* Texto a la derecha */}
-                <div className="right-text">
-                    <h1>Texto a la derecha</h1>
-                    <p>Aquí puedes colocar tu texto.</p>
+                <div className="unase-content">
+                    <img src="../../../public/logo4.png" alt="" />
+                    <div className="unase-text">
+                    <h2 className="h2-manos">Únase a Nosotros</h2>
+                    <p className="p-manos">Ya sea que sea una empresa que busca simplificar su proceso de selección y onboarding o un talentoso candidato en busca de su próximo desafío profesional, Smart Resources es su socio de confianza. Juntos, podemos crear un futuro laboral más brillante y exitoso.
+¡Bienvenido a Smart Resources, donde el talento encuentra su hogar y las empresas encuentran su ventaja competitiva!</p>
+                    </div>
                 </div>
             </div>
 
 
 
-            {/* <Footer /> */}
+            <Footer />
         </div>
     );
 }
