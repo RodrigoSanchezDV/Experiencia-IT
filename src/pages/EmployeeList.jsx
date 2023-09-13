7c77ad29a4d2a15deca0a0ce069a6acc21025c93
Juan Ignacio Grodz <jigrodz@gmail.com>
2023-09-13 18:58:22 -0300

    trabajando en EmployeeList

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "../styles/EmployeeList.css"; // Importa tu archivo CSS para estilos personalizados
 import SearchBar from "../Components/SearchBar";
+import Header from "../Components/Header";
+
 
 export default function EmployeeList() {
   const initialEmployees = [
@@ -60,7 +62,7 @@ export default function EmployeeList() {
     sortedEmployees.sort((a, b) => {
       const nameA = a.lastName.toLowerCase();
       const nameB = b.lastName.toLowerCase();
-  
+
       if (selectedOrder === "asc") {
         return nameA.localeCompare(nameB);
       } else {
@@ -83,46 +85,58 @@ export default function EmployeeList() {
   };
 
   return (
-    <div className="employee-list-container">
-      <h2>Lista de Empleados</h2>
-      <SearchBar onSearch={handleSearch} />
-      <table>
-      <thead>
-  <tr>
-  <th>
-  Apellido{" "}
-  <select onChange={handleSortByLastName}>
-    <option value="asc">Ascendente</option>
-    <option value="desc">Descendente</option>
-  </select>
-</th>
-    <th>Nombre</th>
-    <th>Fecha Ingreso</th>
-    <th>Sector</th>
-    <th>Usuario</th>
-    <th>Email</th>
-    <th>Trabajo</th>
-    {/* Agregar otras columnas según tus necesidades */}
-  </tr>
-</thead>
-<tbody>
-  {employees.map((employee, index) => (
-    <tr key={index}>
-      <td>{employee.lastName}</td>
-      <td>{employee.name}</td>
-      <td>{employee.joinDate}</td>
-      <td>{employee.department}</td>
-      <td>{employee.user}</td>
-      <td>{employee.email}</td>
-      <td>{employee.job}</td>
-      {/* Agregar datos de otras columnas según corresponda */}
-    </tr>
-  ))}
-</tbody>
+    <div>
+      <Header />
+      <div className="employee-list-container">
+        <div className="text-employee-list">
+          <h2 className="h2-employee-list">
+            <div className="left-icon">
+              <i class="fa-solid fa-arrow-left"></i>
+            </div>
+            Lista de Empleados
+          </h2>
+        </div>
 
-      </table>
-      <div>
-        <button>Añadir nuevo empleado</button>
+
+        <SearchBar onSearch={handleSearch} />
+        <table>
+          <thead>
+            <tr>
+              <th>
+                Apellido{" "}
+                <select onChange={handleSortByLastName}>
+                  <option value="asc">Ascendente</option>
+                  <option value="desc">Descendente</option>
+                </select>
+              </th>
+              <th>Nombre</th>
+              <th>Fecha Ingreso</th>
+              <th>Sector</th>
+              <th>Usuario</th>
+              <th>Email</th>
+              <th>Trabajo</th>
+              {/* Agregar otras columnas según tus necesidades */}
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee, index) => (
+              <tr key={index}>
+                <td>{employee.lastName}</td>
+                <td>{employee.name}</td>
+                <td>{employee.joinDate}</td>
+                <td>{employee.department}</td>
+                <td>{employee.user}</td>
+                <td>{employee.email}</td>
+                <td>{employee.job}</td>
+                {/* Agregar datos de otras columnas según corresponda */}
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+        <div>
+          <button>Añadir nuevo empleado</button>
+        </div>
       </div>
     </div>
   );
