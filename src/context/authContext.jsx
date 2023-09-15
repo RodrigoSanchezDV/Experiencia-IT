d5185d622f0269c0e0df7f0f4e46f1380cd4999a
modoliso3k007 <rodriazteca156@gmail.com>
2023-09-15 19:54:04 -0300

    sesion con local storage

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -6,12 +6,14 @@ export const AuthContext = createContext()
 
 const AuthContextComponent = ({ children })=>{
   const navigate = useNavigate()
-  const [user, setUser] = useState({})
-  const [logged, setLogged] = useState(false)
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("userInfo"))||{})
+  const [logged, setLogged] = useState(JSON.parse(localStorage.getItem("logged"))||false)
 
   const handleLogin = ( userLogged ) => {
     setUser(userLogged)
     setLogged(true)
+    localStorage.setItem("userInfo", JSON.stringify(userLogged))
+    localStorage.setItem("logged", JSON.stringify(true))
     /* navigate("/") */
   }
   
@@ -20,6 +22,8 @@ const AuthContextComponent = ({ children })=>{
     console.log(logged)
     setUser({})
     setLogged(false)
+    localStorage.setItem("userInfo")
+    localStorage.setItem("logged")
     console.log("Se cerro la sesion")
     /* navigate("/login") */
   }
@@ -37,3 +41,5 @@ const AuthContextComponent = ({ children })=>{
 }
 
 export default AuthContextComponent
+
+/* http://localhost:5173/ */
